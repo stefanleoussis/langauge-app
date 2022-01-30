407634f18caeef9510a958b0005bc01ceba09150
Stefan <stefanleoussis@gmail.com>
2022-01-30 00:19:53 -0500

    Set up Index.js
    Setup mongoose
    Setup dotenv
    Created Language Schema

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,7 @@
 const { ApolloServer, gql } = require("apollo-server");
-
+require("dotenv").config();
+const mongoose = require("mongoose");
+const dbURL = process.env.dbURL;
 const typeDefs = gql`
   # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
 
@@ -50,3 +52,37 @@ const server = new ApolloServer({
 server.listen().then(({ url }) => {
   console.log(`🚀  Server ready at ${url}`);
 });
+mongoose.connect(dbURL);
+
+// First we create the schema
+const languageSchema = new mongoose.Schema({
+  name: String,
+});
+
+// Documentation Link: https://mongoosejs.com/docs/guide.html
+// Creates a method accesible to language Schema
+languageSchema.methods.phrase = function phrase() {
+  const greeting = this.name
+    ? "We are speaking" + this.name
+    : "I don't know this Language";
+  console.log(greeting);
+};
+
+// then we create the model from the schema
+const Lanugage = mongoose.model("Language", languageSchema);
+
+// now we create document using our model
+const english = new Lanugage({ name: "English" });
+
+// Saves the document to the database
+english.save();
+
+// Calls the method
+english.phrase();
+async function callLanuages() {
+  // finds all documents in the database
+  const random = await Lanugage.find();
+  console.log(random);
+}
+
+callLanuages();
