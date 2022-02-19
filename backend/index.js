5cd3fda29698a2e61f7fbac2d4b5b572b4b8f08f
Stefan <stefanleoussis@gmail.com>
2022-02-19 01:45:34 -0500

    Set up phrase query that returns a phrase

--- backend/index.js
+++ backend/index.js
@@ -6,35 +6,37 @@ const typeDefs = gql`
   # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
 
   # This "Book" type defines the queryable fields for every book in our data source.
-  type Book {
-    title: String
-    author: String
+  type Phrase {
+    sentence: String
   }
 
   # The "Query" type is special: it lists all of the available queries that
   # clients can execute, along with the return type for each. In this
   # case, the "books" query returns an array of zero or more Books (defined above).
   type Query {
-    books: [Book]
+    phrase: String
   }
 `;
 
-const books = [
-  {
-    title: "The Awakening",
-    author: "Kate Chopin",
-  },
-  {
-    title: "City of Glass",
-    author: "Paul Auster",
-  },
+const englishPhrases = [
+  "Nice to meet you",
+  "Where are you from?",
+  "What do you do?",
+  "Do you have Facebook?",
+  "Thanks so much.",
+  "Excuse me.",
+  "What do you think?",
 ];
 
+const phrasesEnglish = "Hello";
 // Resolvers define the technique for fetching the types defined in the
 // schema. This resolver retrieves books from the "books" array above.
+function englishPhrase() {
+  return englishPhrases[0];
+}
 const resolvers = {
   Query: {
-    books: () => books,
+    phrase: () => englishPhrase(),
   },
 };
 
@@ -44,7 +46,7 @@ const server = new ApolloServer({
   typeDefs,
   resolvers,
   context: async ({ req }) => {
-    console.log(req.body.operationName);
+    // console.log(req.body.operationName);
   },
 });
 
@@ -52,37 +54,37 @@ const server = new ApolloServer({
 server.listen().then(({ url }) => {
   console.log(`🚀  Server ready at ${url}`);
 });
-mongoose.connect(dbURL);
+// mongoose.connect(dbURL);
 
-// First we create the schema
-const languageSchema = new mongoose.Schema({
-  name: String,
-});
+// // First we create the schema
+// const languageSchema = new mongoose.Schema({
+//   name: String,
+// });
 
-// Documentation Link: https://mongoosejs.com/docs/guide.html
-// Creates a method accesible to language Schema
-languageSchema.methods.phrase = function phrase() {
-  const greeting = this.name
-    ? "We are speaking" + this.name
-    : "I don't know this Language";
-  console.log(greeting);
-};
+// // Documentation Link: https://mongoosejs.com/docs/guide.html
+// // Creates a method accesible to language Schema
+// languageSchema.methods.phrase = function phrase() {
+//   const greeting = this.name
+//     ? "We are speaking" + this.name
+//     : "I don't know this Language";
+//   console.log(greeting);
+// };
 
-// then we create the model from the schema
-const Lanugage = mongoose.model("Language", languageSchema);
+// // then we create the model from the schema
+// const Lanugage = mongoose.model("Language", languageSchema);
 
-// now we create document using our model
-const english = new Lanugage({ name: "English" });
+// // now we create document using our model
+// const english = new Lanugage({ name: "English" });
 
-// Saves the document to the database
-english.save();
+// // Saves the document to the database
+// english.save();
 
-// Calls the method
-english.phrase();
-async function callLanuages() {
-  // finds all documents in the database
-  const random = await Lanugage.find();
-  console.log(random);
-}
+// // Calls the method
+// english.phrase();
+// async function callLanuages() {
+//   // finds all documents in the database
+//   const random = await Lanugage.find();
+//   console.log(random);
+// }
 
-callLanuages();
+// callLanuages();
